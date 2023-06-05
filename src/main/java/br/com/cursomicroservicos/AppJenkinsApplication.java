dc863c987bc0c21853dd2fcb70bc8874d8013874
Muehlner <muehlner.arao@gmail.com>
2023-06-05 03:21:20 -0300

    commit inicial

---
+++ src/main/java/br/com/cursomicroservicos/AppJenkinsApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursomicroservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/cursomicroservicos/JenkinsController.java
@@ -0,0 +1,18 @@
+package br.com.cursomicroservicos;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+    @GetMapping
+    public ResponseEntity<String> get(){
+        System.out.println("API FUNCIONANDO");
+        return ResponseEntity.ok("Chamando API - Teste Jenkins!");
+    }
+}
+
